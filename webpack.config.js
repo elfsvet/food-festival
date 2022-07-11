3a2bf96c369814803a5adb547ea1c0c740bf0706
elfsvet <stepanmatysik@gmail.com>
2022-07-11 18:17:08 -0400

    finished with 19.3.7

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     // entry for webpack
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     // output for webpack
     output: {
         path: path.resolve(__dirname, 'dist'),
         // the file name is going to be main.bundle.js
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     // mode for webpack to not let it run in production mode because it by default.
     plugins: [
@@ -16,8 +46,11 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
-    mode:'development'
+    mode: 'development'
 
 };
 
