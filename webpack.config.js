8597a89eac4b609e79a2a0b99c36b5f427fe0a52
elfsvet <stepanmatysik@gmail.com>
2022-07-11 13:59:25 -0400

    run a lighthouse audit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {};
+module.exports = {
+    // entry for webpack
+    entry: './assets/js/script.js',
+    // output for webpack
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        // the file name is going to be main.bundle.js
+        filename: 'main.bundle.js'
+    },
+    // mode for webpack to not let it run in production mode because it by default.
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
+    mode:'development'
+
+};
 
 // For a basic configuration, we need to provide webpack with three properties
